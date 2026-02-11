d96b939a4007e49eca675bf1e2de25cbbca286fc
oaa24u370 <oaa24u370@student.bmstu.ru>
2026-02-11 22:26:41 +0300

    Corrections were made

--- source/fibonacciGenerator.js
+++ source/fibonacciGenerator.js
@@ -8,18 +8,26 @@
  * @yields {Number} Следующее число в последовательности Фибоначчи
  */
 function* fibonacciGenerator(number) {
-    if ((!Number.isInteger(number) && number !== Infinity) 
-        || number <= 0) return;
+    if (!Number.isInteger(number) && number !== Infinity) {
+        return;
+    }
+    if (number <= 0) {
+        return;
+    }
 
     yield 0;
-    if (number == 1) return;
+    if (number == 1) {
+        return;
+    }
 
     yield 1;
-    if (number == 2) return;
+    if (number == 2) {
+        return;
+    }
 
     let a = 0,
         b = 1;
-    for (let i = 0; i + 2 < number; ++i) {
+    for (let i = 2; i < number; ++i) {
         [a, b] = [b, a + b]
         yield b;
     }
